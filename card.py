428f8c2e8747f925ad2127a4e9e8b5126876330a
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2024-08-21 09:32:26 +0000

    editted card.py and main.py

--- card.py
+++ card.py
@@ -1,4 +1,5 @@
 CARD_NAMES = ["Jester", "Dalmuti", "Archbishop", "Earl Marshal", "Baroness", "Abbess", "Knight", "Seamstress", "Mason", "Cook", "Shepherdess", "Stonecutter", "Peasant"]
+CARD_NUMBERS = [13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 class Card:
     def __init__(self, card_rank):
--- main.py
+++ main.py
@@ -1,37 +1,6 @@
 import random
-
-CARD_NAMES = ["Jester", "Dalmuti", "Archbishop", "Earl Marshal", "Baroness", "Abbess", "Knight", "Seamstress", "Mason", "Cook", "Shepherdess", "Stonecutter", "Peasant"]
-CARD_NUMBERS = [13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-class Card:
-    def __init__(self, card_rank):
-        self.card_rank = card_rank
-        self.card_name = CARD_NAMES[card_rank]
-
-    def __str__(self):
-        return(f'{self.card_rank}. {self.card_name}')
-    
-    def __repr__(self):
-        return(f'{self.card_rank}. {self.card_name}')
-
-class Player:
-    def __init__(self, name, cards) -> None:
-        self.name = name
-        self.cards = cards
-    def __str__(self):
-        return(self.name)
-    def __repr__(self):
-        return(self.name)
-
-    def get_cards(self):
-        return self.cards
-    
-    def use_card(self, card_rank, card_number):
-        if self.cards[card_rank] >= card_number:
-            self.cards[card_rank] -= card_number
-        else: print("There isn't enough cards.")
-    def update_cards(self, cards):
-        self.cards = cards
+from card import Card
+from player import Player
     
 # Go back to main
 def go_back_to_main(): return
