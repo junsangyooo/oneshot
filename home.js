d42f5c234ce6bfd5265ce5fc8b51ac353b0a3829
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2024-08-21 13:25:16 +0000

    modified home.js

--- home.js
+++ home.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const gameTypeField = document.getElementById('gameType');
     const roomTypeField = document.getElementById('roomType');
     const roomNameField = document.getElementById('roomName');
-
     gameTypeField.addEventListener('change', function() {
         const gameType = gameTypeField.value;
         if (gameType === 'multiplayer') {
