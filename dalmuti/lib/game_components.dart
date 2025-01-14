8c0e9c1f95c329deb82a1655fc01a317228a0d2c
Jun Yoo <70479629+junsangyooo@users.noreply.github.com>
2025-01-14 10:57:00 +0000

    updated game components

--- dalmuti/lib/game_components.dart
+++ dalmuti/lib/game_components.dart
@@ -1,26 +1,42 @@
 import 'package:flutter/material.dart';
 
-final Map<int, String> CARD_NAMES = {1: "Dalmuti", 2:"Archbishop", 3: "Earl Marshal", 4: "Baroness", 5: "Abbess", 6: "Knight", 
-              7: "Seamstress", 8: "Mason", 9: "Cook", 10: "Shepherdess", 11: "Stonecutter", 12: "Peasant", 13: "Jester"}
-/workspaces/The-Great-Dalmuti/assets/Cook.png
+final Map<int, String> cards = {1: "Dalmuti", 2:"Archbishop", 3: "Earl Marshal", 4: "Baroness", 5: "Abbess", 6: "Knight", 
+              7: "Seamstress", 8: "Mason", 9: "Cook", 10: "Shepherdess", 11: "Stonecutter", 12: "Peasant", 13: "Jester"};
+
 class Card {
   final int rank;
   final Image img;
 
-  Card(this.rank, this.img)
-  Card.unlaunched(int rank) : this(rank, Image.asset('../assets/${CARD_NAMES[rank]}.png/'));
-}
+  Card(this.rank, this.img);
+  Card.unlaunched(int rank) : this(rank, Image.asset('assets/${cards[rank]}.png'));
 
-class Hand {
-  List<Card>
+  Image getImage(){
+    return img;
+  }
 }
 
 class Player {
   final String name;
   bool isComputer;
-  Hand hand;
-  int? curRank;
+  int curRank;
+  List<Card> hand;
 
-  Player(this.name, this.isComputer, this.hand);
-  Player.unlaunched(String name) : this(name, false, Hand());
+  Player(this.name, this.isComputer, this.curRank, this.hand);
+  Player.unlaunched(String name) : this(name, false, 0, []);
+
+  void setRank(int n) {
+    curRank = n;
+  }
+  int getRank() {
+    return curRank;
+  }
+  void setHand(List<Card> deck){
+    hand = deck;
+  }
+  List<Card> getHand(){
+    return hand;
+  }
+  void playCard(Card card){
+    hand.remove(card);
+  }
 }
